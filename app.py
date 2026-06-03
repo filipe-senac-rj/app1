aa113fa8e3847ddfb8493291a61d072267b58e78
Filipe Dutra <filipe.senac.rj@gmail.com>
2026-06-03 18:15:47 -0300

    deploy automático 123

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def render_sidebar() -> str:
     user  = st.session_state.get("_user", {})
 
     with st.sidebar:
-        st.markdown("## 🏢 MXM Integrador - deploy automático")
+        st.markdown("## 🏢 MXM Integrador - deploy automático 123")
         st.markdown("---")
 
         # Garante que a página atual é válida para o perfil
